87a73cca49882b55c1c5f5b28ab6bcabb0c7547c
lomnom <57130071+lomnom@users.noreply.github.com>
2021-06-08 19:42:00 +0800

    Update spam.sh

--- spam.sh
+++ spam.sh
@@ -1,1 +1,15 @@
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
+brew list python3 || brew install python3 #install python3
+
+if python3 -c "import pyautogui" &> /dev/null; then
+    echo 'all good'
+else
+    echo 'still needs pyautogui!'
+    pip3 install pyautogui
+fi
+python3 spam.py
